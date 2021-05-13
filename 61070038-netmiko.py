f4947411e3239e9e6e32492f34a7de1094eb1486
kiwkiwkukikuki <61070038@kmitl.ac.th>
2021-05-13 10:54:48 +0700

    2

--- 61070038-netmiko.py
+++ 61070038-netmiko.py
@@ -11,5 +11,11 @@ device_params = {'device_type': 'cisco_ios',
                 }
 
 with ConnectHandler(**device_params) as ssh:
+    ssh.send_command('en')
+    ssh.send_command('conf t')
+    ssh.send_command('int loopback 61070038')
+    ssh.send_command('ip address 192.168.1.1 255.255.255.0')
+    ssh.send_command('no shut')
+    ssh.send_command('end')
     result = ssh.send_command('sh ip int br')
     print(result)
